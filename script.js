a1e2f9634b3a802580a9b77696fa359fc6561eb8
ElieMalike <sparomalick@gmail.com>
2026-05-15 12:58:17 +0200

    exercice JQuery

---
+++ script.js
@@ -0,0 +1,52 @@
+$(".animation1").on("click", () => {
+    $("a").attr("href", "Images/super_mario_bros.jpg")
+})
+
+$(".animation2").on("click", () => {
+    $("a").attr("href", "Images/how_to_train_your_dragon.jpg")
+})
+
+$(".animation3").on("click", () => {
+    $("a").attr("href", "Images/croods_2.jpg")
+})
+
+$(".animation4").on("click", () => {
+    $("a").attr("href", "Images/La-Reine-des-Neiges-1.jpg")
+})
+
+
+$(".reeli").on("click", () => {
+    $("a").attr("href", "Images/les_figures_de_l_ombre.jpg")
+})
+
+$(".reel2").on("click", () => {
+    $("a").attr("href", "Images/bob_marley_one_love.jpg")
+})
+
+$(".reel3").on("click", () => {
+    $("a").attr("href", "Images/Strainght_outta_compton.jpg")
+})
+
+$(".reel4").on("click", () => {
+    $("a").attr("href", "Images/Numéro_24.jpg")
+})
+
+$(".reel5").on("click", () => {
+    $("a").attr("href", "Images/dolemite_is_my_name.webp")
+})
+
+$(".policier1").on("click", () => {
+    $("a").attr("href", "Images/le-flic-de-beverly-hills-1-.jpg.webp")
+})
+
+$(".policier2").on("click", () => {
+    $("a").attr("href", "Images/bad_boys.webp")
+})
+
+$(".policier3").on("click", () => {
+    $("a").attr("href", "Images/Die_Hard.jpg")
+})
+
+$(".policier4").on("click", () => {
+    $("a").attr("href", "Images/Shaft.jpg")
+})
